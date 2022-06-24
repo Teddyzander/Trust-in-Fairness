c951fe96995e326c04bdcc1d9d9f6ff7121755c7
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2022-06-24 10:13:45 +1000

    Fit logistic regression model and measure DP

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import data_util.data_util as data_util
 from sklearn.linear_model import LogisticRegression
+from fairlearn.reductions import DemographicParity
+from fairlearn.metrics import demographic_parity_difference
 
 if __name__ == '__main__':
-
     # get the causality model data
     data, target, sens = data_util.gen_data()
 
@@ -19,7 +20,18 @@ if __name__ == '__main__':
     sensitive = data['D']
     data = data.drop('D', axis=1)
 
+    # split the data into training and testing sets
     x_tr, y_tr, sens_tr, x_te, y_te, sens_te = data_util.split(data, target, sensitive)
 
-    print('stop')
+    # fit model to training data
+    model = LogisticRegression(max_iter=10000)
+    model.fit(x_tr, y_tr)
 
+    print(model.score(x_te, y_te))
+
+    baseline_output = model.predict(x_te)
+    base_fairness = demographic_parity_difference(y_te, baseline_output, sensitive_features=sens_te)
+
+    print(base_fairness)
+
+    print('stop')
